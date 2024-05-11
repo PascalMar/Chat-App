b24ae57fe486ba1b0bebca43afb02ab04323f0f4
Pascal Marienfeld <pa.marienfeld@gmail.com>
2024-05-11 13:55:20 +0200

    Added 2 views for the Chat App

---
+++ components/Start.js
@@ -0,0 +1,118 @@
+import { StyleSheet, View, Text, Button, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+
+
+const Start = ({ navigation }) => {
+    const [name, setName] = useState('');
+    const colors = ['#090C08', '#474056', '#8A95A5', '#B9C6AE'];
+    const [background, setBackground] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require("../images/background-img.png")}
+                resizeMode="cover"
+                style={styles.imageBackground}
+            >
+                <Text>Hello!</Text>
+                <TextInput
+                    style={styles.textInput}
+                    value={name}
+                    onChangeText={setName}
+                    placeholder='Type your username her'
+                />
+                <View style={styles.colorButtonContainer}>
+                    {colors.map((color, index) => (
+                        <TouchableOpacity
+                            key={index}
+                            accessible={true}
+                            accessibilityRole="button"
+                            accessibilityHint="Lets you choose background color for your chat screen"
+                            style={[
+                                styles.colorButton,
+                                { backgroundColor: color },
+                                background === color && styles.selectedColor,
+                            ]}
+                            onPress={() => setBackground(color)}
+                        />
+                    ))}
+                </View>
+                <TouchableOpacity
+                    accessible={true}
+                    accessibilityRole="button"
+                    accessibilityHint="Lets you choose to enter the chat room"
+                    style={styles.button}
+                    onPress={() => navigation.navigate('Chat', { name: name, background: background })}
+                >
+                    <Text style={styles.buttonText}>Start Chatting</Text>
+                </TouchableOpacity>
+            </ImageBackground >
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }, textInput: {
+        width: '88%',
+        borderColor: 'black',
+        borderRadius: 4,
+        color: 'black',
+        fontSize: 16,
+        fontWeight: '500',
+        opacity: 50,
+        padding: 15,
+        borderWidth: 1,
+        marginBottom: 10,
+        placeholderTextColor: 'black',
+    },
+    imageBackground: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: '#757083',
+    },
+    buttonText: {
+        fontSize: 16,    
+        fontWeight: '600',
+        letterSpacing: 0.25,
+        color: '#FFFFFF',
+    },
+    chooseBgColor: {
+        color: '#757083',
+        fontSize: 16,
+        fontWeight: '300',
+        opacity: 100,
+    },
+    colorButtonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingBottom: 10,
+    },
+    colorButton: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        margin: 5
+    },
+    selectedColor: {
+        borderColor: 'white',
+        borderWidth: 2,
+    },
+});
+
+export default Start;
